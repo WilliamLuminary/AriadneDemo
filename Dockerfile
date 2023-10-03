69c1fb5c015618552e97ca560961f5a4a1b37253
William <93109493+WilliamLuminary@users.noreply.github.com>
2023-10-02 22:44:03 -0700

    Dockerize

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ WORKDIR /app
 COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
 
+
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -19,7 +19,6 @@ app.config.from_object(Config)
 # Initialize routes and add them to the app.
 init_app(app, schema)
 
-
 # @app.before_request
 # def check_auth():
 #     if "/graphql" in request.path and "auth" not in request.cookies:
@@ -29,4 +28,4 @@ init_app(app, schema)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
